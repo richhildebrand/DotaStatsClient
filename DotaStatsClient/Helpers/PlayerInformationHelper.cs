c5ca2a8e5a968ae7f13ee06cd175808a0dedb24d
Rich <richhildebrand@gmail.com>
2013-04-27 16:22:16 -0400

    null player information will no longer cause ysod

---
+++ DotaStatsClient/Helpers/PlayerInformationHelper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using DotaSite.Models;
+
+namespace DotaSite.Helpers
+{
+    public class PlayerInformationHelper
+    {
+        public string GetPlayerName(Player player)
+        {
+            if (player.playerInformation != null)
+            {
+                return player.playerInformation.personaname;
+            }
+            else
+            {
+                return player.account_id;
+            }
+        }
+    }
+}
