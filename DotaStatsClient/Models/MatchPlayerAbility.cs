5239020110bb704479d10c4e5c77736c0f242535
Rich <richhildebrand@gmail.com>
2013-05-06 11:17:45 -0400

    added in match navigation

---
+++ DotaStatsClient/Models/MatchPlayerAbility.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace DotaSite.Models
+{
+    public class MatchPlayerAbility
+    {
+        public long match_id { get; set; }
+        public string account_id { get; set; }
+        public int player_slot { get; set; }
+        public int ability { get; set; }
+        public int level { get; set; }
+        public int time { get; set; }
+    }
+}
